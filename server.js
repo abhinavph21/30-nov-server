f61d4cb955f049e17929c3b3abfc4fd7fdb0b6ed
abhinav <83538181+abhinavp21@users.noreply.github.com>
2021-12-01 20:39:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.set('trust proxy', 1)
 //  res.header('Access-Control-Allow-Origin', "http://localhost:3000");
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: "https://myproject-client.netlify.app",
   methods: ["GET", "POST"],
   credentials: true,
   exposedHeaders: ["Set-Cookie"]
@@ -37,7 +37,7 @@ app.use(cors({
 
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Credentials', true);
-   res.header('Access-Control-Allow-Origin', "http://localhost:3000");
+   res.header('Access-Control-Allow-Origin', "https://myproject-client.netlify.app");
 //     res.header("Access-Control-Allow-Origin", "*");
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
