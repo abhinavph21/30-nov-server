e68ac00ddd3d1587a837b4c0d90cfb231bc89284
abhinav <83538181+abhinavp21@users.noreply.github.com>
2021-12-02 12:55:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -35,14 +35,14 @@ app.use(cors({
 //   "http://localhost:3000",
 //   https://myproject-client.netlify.app
 
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Credentials', true);
-   res.header('Access-Control-Allow-Origin', req.headers.origin);
-//     res.header("Access-Control-Allow-Origin", "");
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.header('Access-Control-Allow-Credentials', true);
+//    res.header('Access-Control-Allow-Origin', req.headers.origin);
+// //     res.header("Access-Control-Allow-Origin", "");
+//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 
 app.use(session({
