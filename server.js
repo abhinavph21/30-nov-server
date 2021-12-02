48930d6445684d5ae7ed55a8a9ac2c173bba623a
abhinav <83538181+abhinavp21@users.noreply.github.com>
2021-12-02 12:12:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use(cors({
 
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Credentials', true);
-   res.header('Access-Control-Allow-Origin', "https://myproject-client.netlify.app");
+   res.header('Access-Control-Allow-Origin', req.headers.origin);
 //     res.header("Access-Control-Allow-Origin", "");
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
