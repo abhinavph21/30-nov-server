d1f5b7510d6ab964ffb7d3b108a1b400a3cf173d
abhinav <83538181+abhinavp21@users.noreply.github.com>
2021-12-03 21:08:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ mongoose.connect(process.env.ATLAS_URI, {
 app.use(express.json());
 //  res.header('Access-Control-Allow-Origin', "http://localhost:3000");
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: "https://myproject-client.netlify.app",
   methods: ["GET", "POST"],
   credentials: true,
   exposedHeaders: ["Set-Cookie", "Origin", "X-Requested-With", "Content-Type"]
@@ -36,7 +36,7 @@ app.use(cors({
 app.set('trust proxy', 1)
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "https://myproject-client.netlify.app");
   res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
